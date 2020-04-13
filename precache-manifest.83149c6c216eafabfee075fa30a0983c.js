eac2c0b6748d323be17b9b961bb14b081737f3bf
Mahalakshmi <mahalakshmi.ramanathan@zemosolabs.com>
2020-04-13 23:33:51 +0530

    Updates

---
+++ precache-manifest.83149c6c216eafabfee075fa30a0983c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6baefb28515045776c70a869824ac3a9",
+    "url": "/about-maha/index.html"
+  },
+  {
+    "revision": "36b5ac0bd5d93335fb4e",
+    "url": "/about-maha/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a5f43a9d9fde47e42d1f",
+    "url": "/about-maha/static/js/2.e6a19629.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/about-maha/static/js/2.e6a19629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36b5ac0bd5d93335fb4e",
+    "url": "/about-maha/static/js/main.1a254afe.chunk.js"
+  },
+  {
+    "revision": "7d52963812e002986fa9",
+    "url": "/about-maha/static/js/runtime-main.71784ec6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/about-maha/static/media/logo.5d5d9eef.svg"
+  }
+]);
